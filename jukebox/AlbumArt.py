2d2f0686caf498bd4f41b02a9c159749f5ea8b58
Jeff Bernardis <jeff.bernardis@gmail.com>
2012-01-14 19:35:14 -0500

    Version 1.0c - added try block around PIL logic to trap files that have bad image data

--- jukebox/AlbumArt.py
+++ jukebox/AlbumArt.py
@@ -9,30 +9,30 @@ from cStringIO import StringIO
 def resizeArt(data, width, height):
 	try:
 		pic = img.open(StringIO(data))
+		pic.draft('RGB', (width, height))
+		if pic.mode == 'P':
+			pic = pic.convert()
+		filew, fileh = pic.size
+	
+		if ((filew > width) or (fileh >height)):
+			# determine horizontal and vertical ratios of pic to view
+			ratiow = filew / float(width)
+			ratioh = fileh / float(height)
+			# choose the larger of the 2 ratios
+			ratio = ratiow
+			if ratioh > ratiow:
+				ratio = ratioh
+			# and scale accordingly
+			nheight = int(fileh / ratio)
+			nwidth = int(filew / ratio)
+			pic = pic.resize((nwidth, nheight), img.ANTIALIAS)
+			
+		out = StringIO()
+		pic.save(out, 'JPEG')
+		encoded = out.getvalue()
+		out.close()
+			
+		return encoded
+	
 	except:
 		return None
-	
-	pic.draft('RGB', (width, height))
-	if pic.mode == 'P':
-		pic = pic.convert()
-	filew, fileh = pic.size
-
-	if ((filew > width) or (fileh >height)):
-		# determine horizontal and vertical ratios of pic to view
-		ratiow = filew / float(width)
-		ratioh = fileh / float(height)
-		# choose the larger of the 2 ratios
-		ratio = ratiow
-		if ratioh > ratiow:
-			ratio = ratioh
-		# and scale accordingly
-		nheight = int(fileh / ratio)
-		nwidth = int(filew / ratio)
-		pic = pic.resize((nwidth, nheight), img.ANTIALIAS)
-		
-	out = StringIO()
-	pic.save(out, 'JPEG')
-	encoded = out.getvalue()
-	out.close()
-		
-	return encoded
